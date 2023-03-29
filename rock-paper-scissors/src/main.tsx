9ec808dfd81bfa977ff86d753c71ca5c9ab20442
Aldas <80693733+aldask@users.noreply.github.com>
2023-03-29 20:19:41 +0300

    first commit

---
+++ rock-paper-scissors/src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <h1>ROCK PAPER SCISSORS</h1>
+      <p>Let's get started!</p>
+    </div>
+  );
+};
+
+export default MainPage;
