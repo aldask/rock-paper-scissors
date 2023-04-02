6950e7f4dd26f30296c90fb8f20bee7fa83d1744
Aldas <80693733+aldask@users.noreply.github.com>
2023-04-02 19:04:49 +0300

    small fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ const MainPage: React.FC = () => {
       <div className="mainContainer loaded">
         <h1 className="mainText">ROCK PAPER SCISSORS</h1>
         <div className="infoBox">
-          <p>Welcome to Rock Paper Scissors Mini-game!</p>
+          <p>Welcome to Rock Paper Scissors Mini-Game!</p>
           <p>You will be playing against computer</p>
           <p>Press the button below to start</p>
         </div>
